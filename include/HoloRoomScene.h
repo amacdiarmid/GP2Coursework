d3d246af445d3cf95bb54947c1c1d58f5b1103c0
ricardobarros95 <ricardobarros95@gmail.com>
2015-12-04 15:43:46 +0000

    s

--- include/HoloRoomScene.h
+++ include/HoloRoomScene.h
@@ -26,8 +26,53 @@ public:
 
 	void onKeyDown(SDL_Keycode key) override;
 	void onkeyUp(SDL_Keycode key) override;
-private:
 
+	//ShadowMapping
+	void ShadowFramebuffer();
+	void BuildQuad();
+	void ShadowMapPass();
+	void UpdateLightPerspMVP();
+	void RenderQuad();
+
+	//Post Processing
+	void CreateFrameBuffer();
+	void CleanUpFrameBuffer();
+	void RenderPostQuad();
+
+private:
+	//ShadowMapping 1st pass
+	GLuint depthTexture;
+	GLuint depthMVPLocation;
+	GLuint shadowFBO;
+	mat4 depthMVP;
+	GLuint quadTexture;
+	GLuint quadVBO;
+	//ShadowMapping 2nd pass
+	Shader *shadowMapProgram;
+	GLuint textureSamplerLocation;
+	GLuint MVPLocation;
+	GLuint viewLocation;
+	GLuint modelLocation;
+	GLuint depthBiasLocation;
+	GLuint shadowMapLocation;
+	GLuint lightLocation;
+	vec3 lightInvDir = glm::vec3(0.5, 2, 2);
+	mat4 depthBias;
+	glm::mat4 ModelMatrix = glm::mat4(1.0);
+
+	//PostProcessing
+	GLuint FBOTexture;
+	const int FRAME_BUFFER_WIDTH = 640;
+	const int FRAME_BUFFER_HEIGHT = 480;
+	GLuint FBODepthBuffer;
+	GLuint frameBufferObject;
+	GLuint fullScreenVAO;
+	GLuint fullScreenVBO;
+	GLuint fullScreenShaderProgram;
+	unsigned int lastTicks, currentTicks;
+	float elapsedTime;
+	float totalTime;
+	vec2 screenResolution = vec2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT);
 };
 
 #endif
--- src/HoloRoomScene.cpp
+++ src/HoloRoomScene.cpp
@@ -1,7 +1,6 @@
 #include "HoloRoomScene.h"
 #include "Editor.h"
 
-//int i = 0;
 
 HoloRoomScene::HoloRoomScene()
 {
@@ -24,6 +23,10 @@ void HoloRoomScene::render()
 {
 	//set the clear colour background 
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glViewport(0, 0, 1024, 768);
+
 	//clear the color and depth buffer
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -34,6 +37,23 @@ void HoloRoomScene::render()
 	//backface culling
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
+
+	glUseProgram(shaders["shadowMap"]->getShader());
+	update();
+
+	
+	glUniformMatrix4fv(MVPLocation, 1, GL_FALSE, &input->getMVPmatrix()[0][0]);
+	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, &input->GetViewMatrix()[0][0]);
+	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, &ModelMatrix[0][0]);
+	glUniformMatrix4fv(depthBiasLocation, 1, GL_FALSE, &depthBias[0][0]);
+	glUniform3f(lightLocation, lightInvDir.x, lightInvDir.y, lightInvDir.z);
+
+	glActiveTexture(GL_TEXTURE0);
+
+	glUniform1i(textureSamplerLocation, 0);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, depthTexture);
+	glUniform1i(shadowMapLocation, 1);
 
 	worldObject->render(fustrum);
 
@@ -48,6 +68,7 @@ void HoloRoomScene::render()
 
 void HoloRoomScene::update()
 {
+	GLenum err = GL_NO_ERROR;
 	input->Update();
 
 	//i ++;
@@ -57,16 +78,26 @@ void HoloRoomScene::update()
 
 	worldObject->update(input->getMVPmatrix());
 
-	GLenum err = GL_NO_ERROR;
 	while ((err = glGetError()) != GL_NO_ERROR)
 	{
 		//Process/log the error.
 		cout << "error in updating scene " << err << endl;
 	}
+
+	mat4 biasMatrix(
+		0.5, 0.0, 0.0, 0.0,
+		0.0, 0.5, 0.0, 0.0,
+		0.0, 0.0, 0.5, 0.0,
+		0.5, 0.5, 0.5, 1.0
+		);
+
+	depthBias = biasMatrix * depthMVP;
 }
 
 void HoloRoomScene::createScene()
 {
+	//CreateFrameBuffer(); ENable for post processing
+
 	editor = new Editor(this);
 	debugMode = false;
 
@@ -82,12 +113,26 @@ void HoloRoomScene::createScene()
 	textures["earth"]->createTexture("/EarthTexture.png");
 	textures.insert(pair<string, Texture*>("moon", new Texture("moon")));
 	textures["moon"]->createTexture("/MoonTexture.png");
-
+	
 	//create shaders
 	shaders.insert(pair<string, Shader*>("main", new Shader("main")));
 	shaders["main"]->attatchVertexShader("/textureVS.glsl");
 	shaders["main"]->attatchFragmentShader("/textureFS.glsl");
 	shaders["main"]->createShader();
+	
+	//Shadow Shaders
+	shaders.insert(pair<string, Shader*>("firstPassDepth", new Shader("firstPassDepth")));
+	shaders["firstPassDepth"]->attatchVertexShader("/DepthVS.glsl");
+	shaders["firstPassDepth"]->attatchFragmentShader("/DepthFS.glsl");
+	shaders["firstPassDepth"]->createShader();
+	shaders.insert(pair<string, Shader*>("quadShader", new Shader("quadShader")));
+	shaders["quadShader"]->attatchVertexShader("/quadVS.glsl");
+	shaders["quadShader"]->attatchFragmentShader("/quadFS.txt");
+	shaders["quadShader"]->createShader();
+	shaders.insert(pair<string, Shader*>("shadowMap", new Shader("shadowMap")));
+	shaders["shadowMap"]->attatchVertexShader("/shadowMapVS.glsl");
+	shaders["shadowMap"]->attatchFragmentShader("/shadowMapFS.glsl");
+	shaders["shadowMap"]->createShader();
 
 	//create player/debug cam
 	input = new PlayerController();
@@ -100,18 +145,18 @@ void HoloRoomScene::createScene()
 	loadScene(worldObject, name, this);
 
 	//uncomment for world reset
-	//worldObject->addChild(new GameObject("player", worldObject, input));
-	//worldObject->getChild("player")->addComponent(INPUT_COMPONENT);
+	worldObject->addChild(new GameObject("player", worldObject, input));
+	worldObject->getChild("player")->addComponent(INPUT_COMPONENT);
 
-	//worldObject->addChild(new GameObject("sun", worldObject, objects["teapot"], textures["sun"], shaders["main"]));
-	//worldObject->getChild("sun")->addComponent(RENDER_COMPONENT);
+	worldObject->addChild(new GameObject("sun", worldObject, objects["teapot"], textures["sun"], shaders["shadowMap"]));
+	worldObject->getChild("sun")->addComponent(RENDER_COMPONENT);
 
-	//worldObject->getChild("sun")->addChild(new GameObject("earth", worldObject, objects["teapot"], textures["earth"], shaders["main"]));
-	//worldObject->getChild("sun")->getChild("earth")->addComponent(RENDER_COMPONENT);
-	//worldObject->getChild("sun")->getChild("earth")->changePosition(vec3(25, 0, 0));
+	worldObject->getChild("sun")->addChild(new GameObject("earth", worldObject, objects["teapot"], textures["earth"], shaders["shadowMap"]));
+	worldObject->getChild("sun")->getChild("earth")->addComponent(RENDER_COMPONENT);
+	worldObject->getChild("sun")->getChild("earth")->changePosition(vec3(0, 10, 20));
 
-	//worldObject->getChild("sun")->getChild("earth")->addChild(new GameObject("moon", worldObject, objects["teapot"], textures["moon"], shaders["main"]));
-	//worldObject->getChild("sun")->getChild("earth")->getChild("moon")->addComponent(RENDER_COMPONENT);
+	worldObject->getChild("sun")->getChild("earth")->addChild(new GameObject("moon", worldObject, objects["teapot"], textures["moon"], shaders["shadowMap"]));
+	worldObject->getChild("sun")->getChild("earth")->getChild("moon")->addComponent(RENDER_COMPONENT);
 
 	cout << "world: " << worldObject->getName() << " components: ";
 	worldObject->printComponents();
@@ -123,19 +168,140 @@ void HoloRoomScene::createScene()
 		//Process/log the error.
 		cout << "error in creating scene " << err << endl;
 	}
+
+	depthMVPLocation = glGetUniformLocation(shaders["firstPassDepth"]->getShader(), "depthMVP");
+
+	ShadowFramebuffer();
+	BuildQuad();
+
+	GLuint currentShader = shaders["shadowMap"]->getShader();
+
+	textureSamplerLocation = glGetUniformLocation(currentShader, "texture0");
+	MVPLocation = glGetUniformLocation(currentShader, "MVP");
+	viewLocation = glGetUniformLocation(currentShader, "V");
+	modelLocation = glGetUniformLocation(currentShader, "M");
+	depthBiasLocation = glGetUniformLocation(currentShader, "DepthBiasMVP");
+	shadowMapLocation = glGetUniformLocation(currentShader, "shadowMap");
+	lightLocation = glGetUniformLocation(currentShader, "LightInvDirection");
+
+}
+
+void HoloRoomScene::ShadowFramebuffer()
+{
+	// The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
+	glGenFramebuffers(1, &shadowFBO);
+	glBindFramebuffer(GL_FRAMEBUFFER, shadowFBO);
+
+	// Depth texture. Slower than a depth buffer, but you can sample it later in your shader
+	glGenTextures(1, &depthTexture);
+	glBindTexture(GL_TEXTURE_2D, depthTexture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, 1024, 1024, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthTexture, 0);
+
+	glDrawBuffer(GL_NONE); // No color buffer is drawn to.
+	glReadBuffer(GL_NONE);
+
+	GLenum status;
+	if ((status = glCheckFramebufferStatus(
+		GL_FRAMEBUFFER)) !=
+		GL_FRAMEBUFFER_COMPLETE)	{
+		cout << "Issue	with	Framebuffers" << endl;
+	}
+
+}
+
+void HoloRoomScene::BuildQuad()
+{
+	const GLfloat g_quad_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		-1.0f, 1.0f, 0.0f,
+		-1.0f, 1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+	};
+
+	glGenBuffers(1, &quadVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_quad_vertex_buffer_data), g_quad_vertex_buffer_data, GL_STATIC_DRAW);
+
+	quadTexture = glGetUniformLocation(shaders["quadShader"]->getShader(), "texture");
+}
+
+void HoloRoomScene::ShadowMapPass()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, shadowFBO);
+	glViewport(0, 0, 1024, 1024);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glClear(GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(shaders["firstPassDepth"]->getShader());
+
+	UpdateLightPerspMVP();
+
+	glUniformMatrix4fv(depthMVPLocation, 1, GL_FALSE, &depthMVP[0][0]);
+
+	worldObject->render(fustrum);
+}
+
+void HoloRoomScene::UpdateLightPerspMVP()
+{
+	// Compute the MVP matrix from the light's point of view
+	mat4 depthProjectionMatrix = glm::ortho<float>(-10, 10, -10, 10, -10, 20);
+	mat4 depthViewMatrix = glm::lookAt(lightInvDir, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	mat4 depthModelMatrix = glm::mat4(1.0);
+	depthMVP = depthProjectionMatrix * depthViewMatrix * depthModelMatrix;
+}
+
+void HoloRoomScene::RenderQuad()
+{
+	glViewport(0, 0, 512, 512);
+
+	// Use our shader
+	glUseProgram(shaders["quadShader"]->getShader());
+
+	// Bind our texture in Texture Unit 0
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, depthTexture);
+	// Set our "renderedTexture" sampler to user Texture Unit 0
+	glUniform1i(textureSamplerLocation, 0);
+
+	// 1rst attribute buffer : vertices
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+	glVertexAttribPointer(
+		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+		);
+
+	glDisableVertexAttribArray(0);
+
 }
 
 void HoloRoomScene::destroyScene()
 {
 	shaders["main"]->cleanUp();
+	shaders["shadowMap"]->cleanUp();	
 	objects["teapot"]->cleanUp();
 }
 
 void HoloRoomScene::SceneLoop()
 {
 	windowLoop();
-	update();
+	ShadowMapPass();
 	render();
+	//RenderQuad();
+	//RenderPostQuad();
 }
 
 GameObject *HoloRoomScene::getGameObject(string command)
@@ -157,7 +323,6 @@ GameObject *HoloRoomScene::getWorldObject()
 {
 	return worldObject;
 }
-
 
 Object *HoloRoomScene::getObject(string command)
 {
@@ -204,7 +369,6 @@ Shader *HoloRoomScene::getShader(string command)
 	return tempShader;
 }
 
-
 void HoloRoomScene::onKeyDown(SDL_Keycode key)
 {
 	cout << "Key down " << key << endl;
@@ -236,6 +400,7 @@ void HoloRoomScene::onKeyDown(SDL_Keycode key)
 		break;
 	}
 }
+
 void HoloRoomScene::onkeyUp(SDL_Keycode key)
 {
 	cout << "Key up " << key << endl;
@@ -244,4 +409,116 @@ void HoloRoomScene::onkeyUp(SDL_Keycode key)
 string HoloRoomScene::getName()
 {
 	return name;
+}
+
+void HoloRoomScene::CreateFrameBuffer()
+{
+	glActiveTexture(GL_TEXTURE0);
+	glGenTextures(1, &FBOTexture);
+	glBindTexture(GL_TEXTURE_2D, FBOTexture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT, 0, GL_RGBA,
+		GL_UNSIGNED_BYTE, NULL);
+
+
+	glGenRenderbuffers(1, &FBODepthBuffer);
+	glBindRenderbuffer(GL_RENDERBUFFER, FBODepthBuffer);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT32, FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT);
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+	glGenFramebuffers(1, &frameBufferObject);
+	glBindFramebuffer(GL_FRAMEBUFFER, frameBufferObject);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, FBOTexture, 0);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, FBODepthBuffer);
+
+	GLenum status;
+	if ((status = glCheckFramebufferStatus(GL_FRAMEBUFFER)) != GL_FRAMEBUFFER_COMPLETE) {
+		cout << "Issue with Framebuffers" << endl;
+	}
+	float vertices[] = {
+		-1, -1,
+		1, -1,
+		-1, 1,
+		1, 1,
+
+	};
+
+	glGenVertexArrays(1, &fullScreenVAO);
+	glBindVertexArray(fullScreenVAO);
+
+	glGenBuffers(1, &fullScreenVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, fullScreenVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(
+		0,  // attribute
+		2,                  // number of elements per vertex, here (x,y)
+		GL_FLOAT,           // the type of each element
+		GL_FALSE,           // take our values as-is
+		0,                  // no extra data between each position
+		0                   // offset of first element
+		);
+
+	GLuint vertexShaderProgram = 0;
+	string vsPath = ASSET_PATH + SHADER_PATH + "/simplePostProcessVS.glsl";
+	vertexShaderProgram = loadShaderFromFile(vsPath, VERTEX_SHADER);
+	checkForCompilerErrors(vertexShaderProgram);
+
+	GLuint fragmentShaderProgram = 0;
+	string fsPath = ASSET_PATH + SHADER_PATH + "/simplePostProcessFS.glsl";
+	fragmentShaderProgram = loadShaderFromFile(fsPath, FRAGMENT_SHADER);
+	checkForCompilerErrors(fragmentShaderProgram);
+
+	fullScreenShaderProgram = glCreateProgram();
+	glAttachShader(fullScreenShaderProgram, vertexShaderProgram);
+	glAttachShader(fullScreenShaderProgram, fragmentShaderProgram);
+
+	//Link attributes
+	glBindAttribLocation(fullScreenShaderProgram, 0, "vertexPosition");
+
+	glLinkProgram(fullScreenShaderProgram);
+	checkForLinkErrors(fullScreenShaderProgram);
+	//now we can delete the VS & FS Programs
+	glDeleteShader(vertexShaderProgram);
+	glDeleteShader(fragmentShaderProgram);
+}
+
+void HoloRoomScene::CleanUpFrameBuffer()
+{
+	glDeleteProgram(fullScreenShaderProgram);
+	glDeleteBuffers(1, &fullScreenVBO);
+	glDeleteVertexArrays(1, &fullScreenVAO);
+	glDeleteFramebuffers(1, &frameBufferObject);
+	glDeleteRenderbuffers(1, &FBODepthBuffer);
+	glDeleteTextures(1, &FBOTexture);
+}
+
+void HoloRoomScene::RenderPostQuad()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	//Set the clear colour(background)
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	//clear the colour and depth buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(fullScreenShaderProgram);
+
+	GLint textureLocation = glGetUniformLocation(fullScreenShaderProgram, "texture0");
+	GLint timeLocation = glGetUniformLocation(fullScreenShaderProgram, "time");
+	GLint resolutionLocation = glGetUniformLocation(fullScreenShaderProgram, "resolution");
+
+	glUniform1f(timeLocation, totalTime);
+	glUniform2fv(resolutionLocation, 1, value_ptr(screenResolution));
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, FBOTexture);
+	glUniform1i(textureLocation, 0);
+
+	glBindVertexArray(fullScreenVAO);
+
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
