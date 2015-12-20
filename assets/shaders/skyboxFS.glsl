6308d7b1dd2ebbc8a074d189affbb1fa47ea96b9
amacdiarmid <amacdiarmid@hotmail.co.uk>
2015-12-20 17:31:51 +0000

    cube map is a red box

--- assets/shaders/skyboxFS.glsl
+++ assets/shaders/skyboxFS.glsl
@@ -6,6 +6,6 @@ uniform samplerCube cubeTexture;
 
 void main()
 {
-	FragColour = texture(cubeTexture, vertexTexCoordsOut);
-	//FragColour = vec4(1.0f, 0.0f, 0.0f, 0.0f);
+	//FragColour = texture(cubeTexture, vertexTexCoordsOut);
+	FragColour = vec4(1.0f, 0.0f, 0.0f, 0.0f);
 }
